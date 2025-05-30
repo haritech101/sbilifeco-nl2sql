8890bbf6f99cf68db428aaacec76e8bfc424bd99
Harikrishna Natrajan <hari@tech101.in>
2025-05-30 15:50:37 +0530

    QueryRequest struct that is specific to the /queries request

--- cp/paths/query_flow/src/sbilifeco/cp/query_flow/paths.py
+++ cp/paths/query_flow/src/sbilifeco/cp/query_flow/paths.py
@@ -1,3 +1,11 @@
+from pydantic import BaseModel
+
+
 class Paths:
     BASE = "/api/v1/query-flow"
     QUERIES = BASE + "/queries"
+
+
+class QueryRequest(BaseModel):
+    db_id: str
+    question: str
