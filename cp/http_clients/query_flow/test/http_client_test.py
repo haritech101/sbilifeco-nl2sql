5a3d639c835c63fed4c4aefc63c9b3cd0e3f310e
Harikrishna Natrajan <hari@tech101.in>
2025-06-03 15:19:53 +0530

    Code to test query flow both as a gateway and as a microservice

--- cp/http_clients/query_flow/test/http_client_test.py
+++ cp/http_clients/query_flow/test/http_client_test.py
@@ -8,9 +8,13 @@ from sbilifeco.cp.metadata_storage.http_client import MetadataStorageHttpClient
 
 
 class HttpClientTest(IsolatedAsyncioTestCase):
+    LOCATION = "standalone"  # "inbuilt" "standalone"
     LLM_PORT = 11003
     STORAGE_PORT = 11001
-    FLOW_PORT = 11004
+    if LOCATION == "inbuilt":
+        FLOW_PORT = 9000
+    else:
+        FLOW_PORT = 11004
 
     async def asyncSetUp(self) -> None:
         self.llm = LLMHttpClient()
@@ -22,16 +26,17 @@ class HttpClientTest(IsolatedAsyncioTestCase):
         self.flow = QueryFlow()
         self.flow.set_llm(self.llm).set_metadata_storage(self.storage)
 
-        self.microservice = QueryFlowMicroservice()
-        self.microservice.set_query_flow(self.flow).set_http_port(self.FLOW_PORT)
-        await self.microservice.listen()
+        if self.LOCATION == "inbuilt":
+            self.microservice = QueryFlowMicroservice()
+            self.microservice.set_query_flow(self.flow).set_http_port(self.FLOW_PORT)
+            await self.microservice.listen()
 
         self.client = QueryFlowHttpClient()
         self.client.set_proto("http").set_host("localhost").set_port(self.FLOW_PORT)
 
     async def asyncTearDown(self) -> None:
-        await self.microservice.stop()
-        return await super().asyncTearDown()
+        if self.LOCATION == "inbuilt":
+            await self.microservice.stop()
 
     async def test_query(self):
         # Arrange
@@ -40,13 +45,13 @@ class HttpClientTest(IsolatedAsyncioTestCase):
         assert dbs_response.payload is not None, "Database list is null"
         db = dbs_response.payload[0]
 
-        question = "How much new business was created last month?"
+        question = "What is the real time cashiering for the current month in Mumbai, Delhi and Chennai?"
 
         # Act
         response = await self.client.query(db.id, question)
 
         # Assert
-        self.assertTrue(response.is_success)
+        self.assertTrue(response.is_success, response.message)
         assert response.payload is not None
         print(response.payload)
         self.assertIn("select", response.payload.lower())
