b6f63f153f4b5b4f3ea1caef847e50f5b731855e
Harikrishna Natrajan <hari@tech101.in>
2025-06-17 14:30:41 +0530

    Using env vars for proto and host in query flow

--- bin/query_flow/envvars.py
+++ bin/query_flow/envvars.py
@@ -1,10 +1,18 @@
 class EnvVars:
     http_port = "HTTP_PORT"
+    llm_proto = "LLM_PROTO"
+    llm_host = "LLM_HOST"
     llm_port = "LLM_PORT"
+    metadata_storage_proto = "METADATA_STORAGE_PROTO"
+    metadata_storage_host = "METADATA_STORAGE_HOST"
     metadata_storage_port = "METADATA_STORAGE_PORT"
 
 
 class Defaults:
     http_port = "80"
+    llm_proto = "http"
+    llm_host = "localhost"
     llm_port = "80"
+    metadata_storage_proto = "http"
+    metadata_storage_host = "localhost"
     metadata_storage_port = "80"
