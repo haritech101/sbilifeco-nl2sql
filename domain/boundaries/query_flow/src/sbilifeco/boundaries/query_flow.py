29087bf3dd7c2a107e04228672faa22648d468a6
Harikrishna Natrajan <hari@tech101.in>
2025-07-31 09:24:09 +0530

    Contract method to reset query flow, so that it gets ready for a new context of querying

--- domain/boundaries/query_flow/src/sbilifeco/boundaries/query_flow.py
+++ domain/boundaries/query_flow/src/sbilifeco/boundaries/query_flow.py
@@ -5,3 +5,6 @@ from sbilifeco.models.base import Response
 class IQueryFlow(Protocol):
     async def query(self, dbId: str, question: str) -> Response[str]:
         raise NotImplementedError()
+
+    async def reset(self) -> Response[None]:
+        raise NotImplementedError()
