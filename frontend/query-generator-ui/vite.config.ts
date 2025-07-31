030d8f4cd127a493dcfd0385c880f94a9a7693d0
Harikrishna Natrajan <hari@tech101.in>
2025-07-31 12:16:00 +0530

    Determine running port from env before defaulting to 80

--- frontend/query-generator-ui/vite.config.ts
+++ frontend/query-generator-ui/vite.config.ts
@@ -1,5 +1,7 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+
+const theEnv = loadEnv("", "");
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,6 +9,6 @@ export default defineConfig({
     server: {
         allowedHosts: ["all"],
         host: "0.0.0.0",
-        port: 80,
+        port: parseInt(theEnv?.VITE_APP_PORT || "80"),
     },
 });
