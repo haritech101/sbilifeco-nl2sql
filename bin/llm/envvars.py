22c927809dc59a801dd1f5ee37fbbd9625e1a2ea
Harikrishna Natrajan <hari@tech101.in>
2025-08-04 14:56:13 +0530

    Initiating the preamble and postamble when the microservice is set up

--- bin/llm/envvars.py
+++ bin/llm/envvars.py
@@ -5,6 +5,8 @@ class EnvVars:
     http_port = "HTTP_PORT"
     llm_model = "LLM_MODEL"
     api_key = "API_KEY"
+    preamble_file = "PREAMBLE_FILE"
+    postamble_file = "POSTAMBLE_FILE"
 
 
 class Defaults:
