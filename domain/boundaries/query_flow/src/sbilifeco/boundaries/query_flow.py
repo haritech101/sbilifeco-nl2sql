71648f51a5fa4cb1d28e9b68c0f1c1e93f96da8d
Harikrishna Natrajan <hari@tech101.in>
2025-08-05 14:08:01 +0530

    Contract methods for session management

--- domain/boundaries/query_flow/src/sbilifeco/boundaries/query_flow.py
+++ domain/boundaries/query_flow/src/sbilifeco/boundaries/query_flow.py
@@ -3,6 +3,15 @@ from sbilifeco.models.base import Response
 
 
 class IQueryFlow(Protocol):
+    async def start_session(self) -> Response[str]:
+        raise NotImplementedError()
+
+    async def stop_session(self) -> Response[None]:
+        raise NotImplementedError()
+
+    async def reset_session(self) -> Response[None]:
+        raise NotImplementedError()
+
     async def query(self, dbId: str, question: str) -> Response[str]:
         raise NotImplementedError()
 
