9c068aa1f530b9c11fe6744be378570b82f43ea0
Harikrishna Natrajan <hari@tech101.in>
2025-08-06 13:22:26 +0530

    Implemented query flow use flow as per the latest contract

--- domain/user_flows/query_flow/src/sbilifeco/user_flows/query_flow.py
+++ domain/user_flows/query_flow/src/sbilifeco/user_flows/query_flow.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from uuid import uuid4
 from sbilifeco.boundaries.metadata_storage import IMetadataStorage
 from sbilifeco.boundaries.llm import ILLM
 from sbilifeco.boundaries.session_data_manager import ISessionDataManager
@@ -36,40 +37,115 @@ class QueryFlow(IQueryFlow):
         self.postamble = postamble
         return self
 
+    async def start_session(self) -> Response[str]:
+        return Response.ok(uuid4().hex)
+
+    async def stop_session(self, session_id: str) -> Response[None]:
+        try:
+            delete_response = await self.session_data_manager.delete_session_data(
+                session_id
+            )
+            if not delete_response.is_success:
+                return Response.fail(delete_response.message, delete_response.code)
+
+            # All good
+            return Response.ok(None)
+        except Exception as e:
+            return Response.error(e)
+
+    async def reset_session(self, session_id: str) -> Response[None]:
+        try:
+            delete_response = await self.session_data_manager.delete_session_data(
+                session_id
+            )
+            if not delete_response.is_success:
+                return Response.fail(delete_response.message, delete_response.code)
+
+            return Response.ok(None)
+        except Exception as e:
+            return Response.error(e)
+
     async def query(self, dbId: str, session_id: str, question: str) -> Response[str]:
         try:
-            # if not self.is_metadata_sent:
-            #     db_response = await self.metadata_storage.get_db(
-            #         dbId,
-            #         with_tables=True,
-            #         with_fields=True,
-            #         with_kpis=True,
-            #         with_additional_info=True,
-            #     )
-            #     if not db_response.is_success:
-            #         return Response.fail(db_response.message, db_response.code)
-            #     db = db_response.payload
-            #     if db is None:
-            #         return Response.fail("Metadata is inexplicably blank", 500)
-
-            #     set_metadata_response = await self.llm.set_metadata(db)
-            #     if not set_metadata_response.is_success:
-            #         return Response.fail(
-            #             set_metadata_response.message, set_metadata_response.code
-            #         )
-
-            #     self.is_metadata_sent = True
-
-            # query_response = await self.llm.generate_sql(question)
-            # if not query_response.is_success:
-            #     return Response.fail(query_response.message, query_response.code)
-            # sql = query_response.payload
-            # if sql is None:
-            #     return Response.fail("LLM did not return a valid SQL query", 500)
-
-            # return Response.ok(sql)
-            raise NotImplementedError(
-                "Query method is not implemented yet. Please implement the logic to handle the query."
+            session_data_response = await self.session_data_manager.get_session_data(
+                session_id
             )
+            if not session_data_response.is_success:
+                return Response.fail(
+                    session_data_response.message, session_data_response.code
+                )
+            if session_data_response.payload is None:
+                return Response.fail("Session data is inexplicably None", 500)
+            session_data = session_data_response.payload
+
+            if session_data == "":
+                db_response = await self.metadata_storage.get_db(
+                    dbId,
+                    with_tables=True,
+                    with_fields=True,
+                    with_kpis=True,
+                    with_additional_info=True,
+                )
+                if not db_response.is_success:
+                    return Response.fail(db_response.message, db_response.code)
+                db = db_response.payload
+                if db is None:
+                    return Response.fail("Metadata is inexplicably blank", 500)
+
+                if self.preamble:
+                    session_data += self.preamble + "\n\n"
+
+                session_data += (
+                    "The query will be based on the following database metadata:\n"
+                )
+
+                session_data += f"Database name: {db.name}\n"
+                session_data += f"Database description: {db.description}\n"
+                if db.tables is not None:
+                    for table in db.tables:
+                        session_data += f"\tTable name: {table.name}\n"
+                        session_data += f"\tTable description: {table.description}\n"
+                        if table.fields is not None:
+                            for field in table.fields:
+                                session_data += f"\t\tField name: {field.name}, type: {field.type}\n"
+                                session_data += (
+                                    f"\t\tField description: {field.description}\n"
+                                )
+                                session_data += f"\t\tOther names for field '{field.name}': {field.aka}\n"
+
+                if db.kpis is not None:
+                    session_data += "KPIs:\n"
+                    for kpi in db.kpis:
+                        session_data += f"\tKPI name: {kpi.name}\n"
+                        session_data += f"\tKPI other names: {kpi.aka}\n"
+                        session_data += f"\tKPI description: {kpi.description}\n"
+                        session_data += f"\tKPI formula: {kpi.formula}\n"
+
+                if db.additional_info:
+                    session_data += (
+                        "Also keep in mind the following additional points.\n"
+                        f"{db.additional_info}\n"
+                    )
+
+                if self.postamble:
+                    session_data += self.postamble + "\n\n"
+
+            session_data += f"\n{question}\n"
+
+            query_response = await self.llm.generate_sql(session_data)
+            if not query_response.is_success:
+                return Response.fail(query_response.message, query_response.code)
+            sql = query_response.payload
+            if sql is None:
+                return Response.fail("LLM did not return a valid SQL query", 500)
+
+            updated_session_data = session_data + f"\n{sql}\n"
+            update_response = await self.session_data_manager.update_session_data(
+                session_id, updated_session_data
+            )
+            if not update_response.is_success:
+                return Response.fail(update_response.message, update_response.code)
+
+            return Response.ok(sql)
         except Exception as e:
             return Response.error(e)
--- domain/user_flows/query_flow/test/flow_test.py
+++ domain/user_flows/query_flow/test/flow_test.py
@@ -3,37 +3,163 @@ import sys
 
 sys.path.append("./src")
 
+from turtle import up, update
 from unittest import IsolatedAsyncioTestCase
 from unittest.mock import AsyncMock, patch
 from sbilifeco.user_flows.query_flow import QueryFlow
 from sbilifeco.boundaries.metadata_storage import IMetadataStorage
 from sbilifeco.boundaries.llm import ILLM
 from sbilifeco.boundaries.session_data_manager import ISessionDataManager
+from sbilifeco.models.base import Response
+from faker import Faker
+from uuid import uuid4
+from sbilifeco.models.db_metadata import DB, Table, Field, KPI
 
 
 class FlowTest(IsolatedAsyncioTestCase):
     async def asyncSetUp(self) -> None:
+        self.faker = Faker()
+        self.session_id = uuid4().hex
+        self.preamble = self.faker.paragraph()
+        self.postamble = self.faker.paragraph()
+        self.random_session_data = self.faker.paragraph()
+        self.question = self.faker.sentence() + "?"
+        self.answer = self.faker.paragraph()
+        self.db_metadata = DB(
+            id=uuid4().hex,
+            name=self.faker.word(),
+            description=self.faker.sentence(),
+        )
+
         self.metadata_storage: IMetadataStorage = AsyncMock(spec=IMetadataStorage)
         self.llm: ILLM = AsyncMock(spec=ILLM)
         self.session_data_manager: ISessionDataManager = AsyncMock(
             spec=ISessionDataManager
         )
 
+        self.patched_get_db = patch.object(
+            self.metadata_storage,
+            "get_db",
+            AsyncMock(return_value=Response.ok(self.db_metadata)),
+        ).start()
+
         self.query_flow = QueryFlow()
+        (
+            self.query_flow.set_metadata_storage(self.metadata_storage)
+            .set_llm(self.llm)
+            .set_session_data_manager(self.session_data_manager)
+            .set_preamble(self.preamble)
+            .set_postamble(self.postamble)
+        )
 
         return await super().asyncSetUp()
 
     async def asyncTearDown(self) -> None:
         return await super().asyncTearDown()
 
-    async def test_query(self) -> None:
+    async def test_start_session(self) -> None:
         # Arrange
+
         # Act
+        response = await self.query_flow.start_session()
+
         # Assert
-        pass
+        self.assertTrue(response.is_success, response.message)
+        assert response.payload is not None
+
+    async def test_stop_session(self) -> None:
+        # Arrange
+        with patch.object(
+            self.session_data_manager,
+            "delete_session_data",
+            AsyncMock(return_value=Response.ok(None)),
+        ) as patched_delete_session_data:
+            # Act
+            response = await self.query_flow.stop_session(self.session_id)
+
+            # Assert
+            self.assertTrue(response.is_success, response.message)
+            patched_delete_session_data.assert_called_once_with(self.session_id)
+
+    async def __test_query(self, initial_session_data: str = "") -> None:
+        # Arrange
+        patched_get_session_data = patch.object(
+            self.session_data_manager,
+            "get_session_data",
+            AsyncMock(return_value=Response.ok(initial_session_data)),
+        ).start()
+
+        patched_update_session_data = patch.object(
+            self.session_data_manager,
+            "update_session_data",
+            AsyncMock(return_value=Response.ok(None)),
+        ).start()
+
+        patched_llm_query = patch.object(
+            self.llm,
+            "generate_sql",
+            AsyncMock(return_value=Response.ok(self.answer)),
+        ).start()
+
+        # Act
+        query_response = await self.query_flow.query(
+            dbId=self.db_metadata.id,
+            session_id=self.session_id,
+            question=self.question,
+        )
+
+        # Assert
+        # Response should be successful and have a non-None payload
+        self.assertTrue(query_response.is_success, query_response.message)
+        assert query_response.payload is not None
+        self.assertEqual(query_response.payload, self.answer)
+
+        # session data manager should have been queried
+        patched_get_session_data.assert_called_once_with(self.session_id)
+
+        if not initial_session_data:
+            # db metadata should have been fetched
+            self.patched_get_db.assert_called_once()
+            param_session_id = patched_get_session_data.call_args[0][0]
+            self.assertEqual(param_session_id, self.session_id)
+
+        # Session data sent to the LLM
+        session_data_for_llm = patched_llm_query.call_args[0][0]
+
+        # should contain the question
+        self.assertIn(self.question, session_data_for_llm)
+
+        # session data manager should have been called with updated session data
+        patched_update_session_data.assert_called_once()
+
+        updated_session_data = patched_update_session_data.call_args[0][1]
+        self.assertIn(self.question, updated_session_data)
+        self.assertIn(self.answer, updated_session_data)
+        if initial_session_data:
+            self.assertIn(initial_session_data, updated_session_data)
+        else:
+            self.assertIn(self.preamble, updated_session_data)
+            self.assertIn(self.postamble, updated_session_data)
+            self.assertIn(self.db_metadata.name, updated_session_data)
+            self.assertIn(self.db_metadata.description, updated_session_data)
+
+    async def test_query_new_session(self) -> None:
+        await self.__test_query()
+
+    async def test_query_continue_session(self) -> None:
+        await self.__test_query(self.random_session_data)
 
     async def test_reset(self) -> None:
         # Arrange
+        patched_delete_session_data = patch.object(
+            self.session_data_manager,
+            "delete_session_data",
+            AsyncMock(return_value=Response.ok(None)),
+        ).start()
+
         # Act
+        response = await self.query_flow.reset_session(self.session_id)
+
         # Assert
-        pass
+        self.assertTrue(response.is_success, response.message)
+        patched_delete_session_data.assert_called_once_with(self.session_id)
