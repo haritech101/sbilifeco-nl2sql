03715d0718d01539dcb95f52adb1f93f1a921f41
Harikrishna Natrajan <hari@tech101.in>
2025-08-07 12:39:01 +0530

    Changes to microservice as per the latest version of Gemini gateway

--- bin/llm/envvars.py
+++ bin/llm/envvars.py
@@ -2,10 +2,8 @@ class EnvVars:
     http_port = "HTTP_PORT"
     llm_model = "LLM_MODEL"
     api_key = "API_KEY"
-    preamble_file = "PREAMBLE_FILE"
-    postamble_file = "POSTAMBLE_FILE"
 
 
 class Defaults:
     http_port = "80"
-    llm_model = "codellama"
+    llm_model = "gemini-1.5-flash"
