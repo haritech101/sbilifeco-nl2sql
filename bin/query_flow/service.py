8b61702437f443b1ea01c259425d8cf0bc56c74f
Harikrishna Natrajan <hari@tech101.in>
2025-09-04 12:19:36 +0530

    Prompt broken into Q-A, rather than preamble-postamble

--- bin/query_flow/service.py
+++ bin/query_flow/service.py
@@ -1,4 +1,6 @@
 from asyncio import run, sleep
+from typing import AsyncGenerator, TextIO
+from webbrowser import get
 from sbilifeco.cp.llm.http_client import LLMHttpClient
 from sbilifeco.cp.metadata_storage.http_client import MetadataStorageHttpClient
 from sbilifeco.cp.session_data_manager.http_client import SessionDataManagerHttpClient
@@ -10,6 +12,12 @@ from envvars import EnvVars, Defaults
 
 
 class QueryFlowMicroservice:
+    prompts = []
+
+    async def _prompts(self) -> AsyncGenerator[str, None]:
+        for prompt in self.prompts:
+            yield prompt
+
     async def run(self) -> None:
         llm_proto = getenv(EnvVars.llm_proto, Defaults.llm_proto)
         llm_host = getenv(EnvVars.llm_host, Defaults.llm_host)
@@ -35,17 +43,10 @@ class QueryFlowMicroservice:
             getenv(EnvVars.session_data_port, Defaults.session_data_port)
         )
 
-        preamble = ""
-        preamble_file = getenv(EnvVars.preamble_file)
-        if preamble_file:
-            with open(preamble_file) as preamble_stream:
-                preamble = preamble_stream.read()
-
-        postamble = ""
-        postamble_file = getenv(EnvVars.postamble_file)
-        if postamble_file:
-            with open(postamble_file) as postamble_stream:
-                postamble = postamble_stream.read()
+        prompts_file = getenv(EnvVars.prompts_file)
+        if prompts_file:
+            prompts_stream = open(prompts_file)
+            self.prompts = prompts_stream.read().split("=====")
 
         flow_port = int(getenv(EnvVars.http_port, Defaults.http_port))
 
@@ -63,7 +64,7 @@ class QueryFlowMicroservice:
         flow = QueryFlow()
         flow.set_llm(llm).set_metadata_storage(storage).set_session_data_manager(
             session_data_manager
-        ).set_preamble(preamble).set_postamble(postamble)
+        ).set_prompts(self._prompts())
 
         microservice = QueryFlowHttpService()
         microservice.set_query_flow(flow).set_http_port(flow_port)
