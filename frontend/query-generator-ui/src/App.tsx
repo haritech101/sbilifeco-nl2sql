356fcb2e04ea4ad154cfd8e01b28ad3bcd60f8d0
Harikrishna Natrajan <hari@tech101.in>
2025-09-15 09:52:47 +0530

    Using markdown and accordingly changing progress indicator

--- frontend/query-generator-ui/src/App.tsx
+++ frontend/query-generator-ui/src/App.tsx
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect, useState } from "react";
 import { useApi, type Schema } from "./ApiProvider";
+import Markdown from "react-markdown";
 
 type ChatMessage = {
     role: "You" | "Me";
@@ -16,6 +17,7 @@ function App() {
     const [chat, setChat] = useState<ChatMessage[]>([]);
     const [currentQuestion, setCurrentQuestion] = useState<string>("");
     const [withThoughts, setWithThoughts] = useState<boolean>(false);
+    const [isSeekingAnswer, setIsSeekingAnswer] = useState<boolean>(false);
 
 
     useEffect(() => {
@@ -46,7 +48,9 @@ function App() {
     }
 
     const handleSubmitMessage = async () => {
-        let updatedChat: ChatMessage[] = [...chat, { role: "You", content: currentQuestion }, { role: "Me", content: "..." }];
+        setIsSeekingAnswer(true);
+
+        let updatedChat: ChatMessage[] = [...chat, { role: "You", content: currentQuestion }];
         setChat(updatedChat);
         setCurrentQuestion("");
 
@@ -61,8 +65,6 @@ function App() {
 
         // Response recieved, either success or failure
 
-        updatedChat.splice(updatedChat.length - 1, 1); // Remove the last "Me: ..." message
-
         const newMessage: ChatMessage = { role: "Me", content: "" };
         if (!response.is_success) {
             newMessage.content = `Error: ${response.message}`;
@@ -76,6 +78,7 @@ function App() {
         ];
 
         setChat(updatedChat);
+        setIsSeekingAnswer(false);
     }
 
     const handleChooseSchema = async (schemaId: string) => {
@@ -126,19 +129,18 @@ function App() {
                     </div>
                 </div>
                 <div className="flex-grow-1 border overflow-auto chat-window d-flex flex-column gap-2 p-3">
-                    {chat.map((message, idx) => (<>
-                        <p key={idx}>
-                            <strong>{message.role}:</strong>{
-                                message.content === "..." ? (
-                                    <div className="spinner-border"></div>
-                                ) : (
-                                    <>{
-                                        message.content.split("\n").map((line, lineIdx) => (<>{line}<br /></>))
-                                    }</>
+                    <Markdown>
+                        {
+                            chat.map(
+                                (message) =>
+                                (
+                                    message.role === "You"
+                                        ? `**You asked:** ${message.content}`
+                                        : `**My response:**\n\n${message.content}`
                                 )
-                            }
-                        </p>
-                    </>))}
+                            ).join("\n\n")
+                        }
+                    </Markdown>
                 </div>
                 <div className="d-flex flex-row gap-2">
                     <textarea value={currentQuestion} className="flex-grow-1 form-control" name="question" onChange={(e) => setCurrentQuestion(e.target.value)}></textarea>
@@ -147,7 +149,7 @@ function App() {
                         name="generate"
                         onClick={() => { handleSubmitMessage() }}
                         disabled={currentQuestion.trim() === "" || chosenSchemaId === ""}
-                    >Generate Query</button>
+                    >{isSeekingAnswer ? <span className='spinner-border'></span> : `Generate Query`}</button>
                 </div>
             </main>
         </div>
