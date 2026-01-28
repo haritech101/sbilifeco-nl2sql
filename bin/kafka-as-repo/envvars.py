4f6b4b988e01c7480e3abd0c7c41a03b16c2b48a
Harikrishna Natrajan <hari@tech101.in>
2026-01-28 14:12:26 +0530

    Microservice to serve kafka as the non-SQL answers repository over HTTP

---
+++ bin/kafka-as-repo/envvars.py
@@ -0,0 +1,16 @@
+class EnvVars:
+    test_type = "TEST_TYPE"
+    http_port = "HTTP_PORT"
+    staging_host = "STAGING_HOST"
+    kafka_url = "KAFKA_URL"
+    kafka_consumer_name = "KAFKA_CONSUMER_NAME"
+    answer_timeout = "ANSWER_TIMEOUT"
+
+
+class Defaults:
+    test_type = "unit"  # or "integration" or "staging"
+    http_port = "80"
+    staging_host = "localhost"
+    kafka_url = "localhost:9092"
+    kafka_consumer_name = "non-sql-answers-consumer"
+    answer_timeout = "5"  # in seconds
