b42f3353efaab6a6134e4468cb5cb6d2e35de4e0
Harikrishna Natrajan <hari@tech101.in>
2026-02-11 14:25:29 +0530

    Script to run a batch of query-flow tests given a YAML file of questions

---
+++ sbin/query-flow-batch-test/envvars.py
@@ -0,0 +1,20 @@
+class EnvVars:
+    test_type = "TEST_TYPE"
+    query_flow_proto = "QUERY_FLOW_PROTO"
+    query_flow_host = "QUERY_FLOW_HOST"
+    query_flow_port = "QUERY_FLOW_PORT"
+    staging_host = "STAGING_HOST"
+    questions_file = "QUESTIONS_FILE"
+    db_id = "DB_ID"
+    answers_file = "ANSWERS_FILE"
+
+
+class Defaults:
+    test_type = "unit"  # or "integration" or "staging"
+    query_flow_proto = "http"
+    query_flow_host = "localhost"
+    query_flow_port = "80"
+    staging_host = "localhost"
+    questions_file = "./questions.yaml"
+    db_id = "nb"
+    answers_file = "./answers.md"
